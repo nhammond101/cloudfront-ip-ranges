04a2f7d5c6e1400c7127b8365cb627a66daf12f7
Nick Hammond <nick@happypengu.in>
2023-02-15 12:24:49 +0000

    refactor: tooling improvements
    eslint, prettier, husky updated, commitizen

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'no-restricted-syntax': 'warn',
+  },
+  root: true,
+  settings: {},
+};
